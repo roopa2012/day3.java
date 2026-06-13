9c1e2673b7bb176e55f4a48d8d3fc06af861c7cb
roopa2012 <rooparam5389367@gmail.com>
2026-06-13 22:28:54 +0530

    Create EvenOrOdd.java

---
+++ EvenOrOdd.java
@@ -0,0 +1,11 @@
+public class EvenOrOdd{
+  static int check(int a){
+    if(n % 2 == 0)
+      return Even;
+    else
+      return Odd;
+  }
+  public static void main(String[] args){
+    Sytem.out.println(check(5));
+  }
+}
