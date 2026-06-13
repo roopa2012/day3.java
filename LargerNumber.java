f759e4ef1252f7c20be0494967cc5c8ff9a6d34f
roopa2012 <rooparam5389367@gmail.com>
2026-06-13 22:15:18 +0530

    Create LargerNumber.java

---
+++ LargerNumber.java
@@ -0,0 +1,11 @@
+public class LargerNumber{
+  static int larger(int a, int b){
+    if(a < b)
+      return b;
+    else
+      return a;
+  }
+  public static void main(String[] args){
+    System.out.println(larger(35,54);
+  }
+}
