30579e85ce59dc7f756e80c33e1b162e51dce246
roopa2012 <rooparam5389367@gmail.com>
2026-06-13 22:24:35 +0530

    Create Factional.java

---
+++ Factional.java
@@ -0,0 +1,12 @@
+public class Factional{
+  static int farc(int a){
+    int f = 1;
+    if(int i = 0; i<=n; i++){
+      f*=i;
+    }
+    return f;
+  }
+  public static void main(String[] args){
+    System.out.println(farc(4));
+  }
+}
